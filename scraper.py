d85ee4423567092e6c7a173f25733b131a816635
Luis Capelo <luiscape@gmail.com>
2014-04-22 22:16:45 -0400

    testing with the aid security database

--- scraper.py
+++ scraper.py
@@ -1,14 +1,22 @@
 import scraperwiki
 import lxml.html
-html = scraperwiki.scrape("http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm")
-root = lxml.html.fromstring(html)
+url = scraperwiki.scrape("https://aidworkersecurity.org/incidents/search")
+root = lxml.html.fromstring(url)
 for tr in root.cssselect("div[align='left'] tr"):
     tds = tr.cssselect("td")
     if len(tds)==12:
         data = {
-            'country' : tds[0].text_content(),
-            'years_in_school' : int(tds[4].text_content())
+            'ID' : tds[0].text_content(),
+            'Month' : tds[1].text_content(),
+            'Day' : tds[2].text_content(),
+            'Year' : int(tds[3].text_content()),
+            'Country' : tds[4].text_content(),
+            'UN' : int(tds[5].text_content()),
+            'INGO' : int(tds[6].text_content())
+
         }
         print data
 
-scraperwiki.sqlite.save(unique_keys=['country'], data=data)
+return data
+
+scraperwiki.sqlite.save(unique_keys=['ID', 'Month', 'Day', 'Year', 'Country', 'UN', 'INGO'], data=data)
